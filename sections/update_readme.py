279ebab5448bc1a79fa5be0f348e321d05d628f8
Pranav Belhekar <pranavbelhekar2002@gmail.com>
2024-01-12 21:06:36 +0530

    update

--- sections/update_readme.py
+++ sections/update_readme.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 # --------------------------------
-
 def get_daily_papers(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
